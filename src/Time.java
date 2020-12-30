32c2b64262733b0fb2e8bf18cd56a0ed64d21664
Malbadr1 <Albadri@gmx.net>
2020-12-30 15:51:33 +0100

    1,,,

--- src/Time.java
+++ src/Time.java
@@ -63,6 +63,19 @@ public class Time {
         System.out.println("After adding 10 mins : " + afterAdding10Mins);
         return "";
     }
+    String getAddmin(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input seconds: ");
+        int seconds = in.nextInt();
+        int p1 = seconds % 60;
+        int p2 = seconds / 60;
+        int p3 = p2 % 60;
+        p2 = p2 / 60;
+        System.out.print( p2 + ":" + p3 + ":" + p1);
+        System.out.print("\n");
+        return "";
+    }
+
 
     @Override
     public String toString() {
@@ -76,6 +89,7 @@ public class Time {
 
         Time time = new Time();
         System.out.println(time.getAddDate());
+        System.out.println(time.getAddmin());
 
     }
 
